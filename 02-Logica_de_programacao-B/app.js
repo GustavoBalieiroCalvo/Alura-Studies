6c31f81afd506cc2507d5bccad7328d0362d9829
Gustavo Balieiro Calvo <gustavobalieirocalvo@protonmail.com>
2025-08-13 10:48:06 -0300

    202508131048

--- 02-Logica_de_programacao-B/app.js
+++ 02-Logica_de_programacao-B/app.js
@@ -1,11 +1,52 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Jogo do Número Secreto";
-
-
+let numeroSecreto = gerarNumeroAleatorio();
 let paragrafo = document.querySelector('p');
+let tentativas = 1;
+
 paragrafo.innerHTML = "Escolha um número entre 1 e 10";
 
-
-function verificarChute() {
-    console.log("O botão foi clicado");
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela("h1", "Jogo do número secreto");
+    exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+}
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    if(chute == numeroSecreto) {
+        exibirTextoNaTela("h1", "Você acertou!");
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`
+        exibirTextoNaTela("p", mensagemTentativas);
+        document.getElementById("reiniciar").removeAttribute("disabled");
+    } else {
+        if(chute > numeroSecreto) {
+            exibirTextoNaTela("p", "O número secreto é menor");
+        } else {
+            exibirTextoNaTela("p", "O número secreto é maior");
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+exibirTextoNaTela('h1', "Jogo do Número Secreto");
+exibirTextoNaTela('p', "Escolha um número de 1 a 10");
+
+function limparCampo() {
+     chute = document.querySelector("input");
+     chute.value = "";
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial()
+};
