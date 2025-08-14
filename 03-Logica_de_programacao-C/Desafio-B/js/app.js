dcdfd7a32c088bed60e90948ea277bee1994e5a2
Gustavo Balieiro Calvo <gustavobalieirocalvo@protonmail.com>
2025-08-14 17:26:34 -0300

    ultima

--- 03-Logica_de_programacao-C/Desafio-B/js/app.js
+++ 03-Logica_de_programacao-C/Desafio-B/js/app.js
@@ -1,12 +1,18 @@
 function alterarStatus(n) {
     let item = document.getElementById(`game-${n}`);
+    let imagem = item.querySelector("div");
     let botao = item.querySelector("a");
-
     if(botao.classList.contains("dashboard__item__button--return")) {
+        if(confirm("Tem certeza que gostaria de devolver o Game?")){
         botao.textContent = "Alugar";
         botao.classList.remove("dashboard__item__button--return");
+        imagem.classList.remove("dashboard__item__img--rented");
+        } else {
+            return;
+        }
     } else {
         botao.textContent = "Devolver";
         botao.classList.add("dashboard__item__button--return");
+        imagem.classList.add("dashboard__item__img--rented");
     }
 }
